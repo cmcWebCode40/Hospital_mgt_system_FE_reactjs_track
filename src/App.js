6276256068d1e17e340a7cfed5c1230f57227f78
unknown <infinity_michael@yahoo.com>
2020-03-16 14:34:21 -0700

    project setup for teammates

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layouts from './layouts/Layouts';
+import "./sass/main.scss"
+
+function App() {
+  return (
+    <div>
+      <Layouts/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/Layouts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// NB : All components are wrapped in the layout component
+
+const Layouts = () => {
+    return (
+        <div>
+           <h1>Hey Guys lets get started!</h1> 
+        </div>
+    )
+}
+
+export default Layouts
