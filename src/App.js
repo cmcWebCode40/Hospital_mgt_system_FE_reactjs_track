bf01a775d15b1713280bdd04f6ca24fcc0e2bc14
Toyin Adesina <toyinadesina60@gmail.com>
2020-03-18 17:32:20 +0100

    SignUp landing page designed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Layouts from './layouts/Layouts';
-import "./sass/main.scss"
+import "./sass/main.scss";
+import "./sass/signup_main.css";
+import SignUp from './components/header/signUpComponents/SignUp'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-      <Layouts/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Layouts/>
+        <Switch>
+          <Route exact path="/"/>
+          <Route path="/signup" component={SignUp} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
