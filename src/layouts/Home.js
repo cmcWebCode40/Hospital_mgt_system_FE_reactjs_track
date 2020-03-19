34eeb899db1b54567d8cece295d72a43136906f5
unknown <infinity_michael@yahoo.com>
2020-03-19 07:08:06 -0700

    setup the comtentpage for display

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/header/Header';
-import Content from '../components/contentpage/Content'
+import Content from '../components/contentpage/Content';
+import Card from '../components/contentpage/Card';
 
 const Home = () => {
     return (
         <div>
             <Header/>
             <Content/>
+            <Card/>
         </div>
     )
 }
