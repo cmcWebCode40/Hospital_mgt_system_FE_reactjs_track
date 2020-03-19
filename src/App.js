ef365eea8f7e935c2177bfadb78952c6a376da80
Toyin Adesina <toyinadesina60@gmail.com>
2020-03-19 16:56:48 +0100

    SignUp landing added to the layout component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import Layouts from './layouts/Layouts';
 import "./sass/main.scss";
-import "./sass/signup_main.css";
-import SignUp from './components/header/signUpComponents/SignUp'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Layouts/>
-        <Switch>
-          <Route exact path="/"/>
-          <Route path="/signup" component={SignUp} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Layouts/>
+    </div>
   );
 }
 
