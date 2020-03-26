9abb0768cde44b8590160abc42dd53c026a45ecd
unknown <infinity_michael@yahoo.com>
2020-03-26 06:42:39 -0700

    added the context api for sharing props

---
+++ src/shared/ContexApi.js
@@ -0,0 +1,16 @@
+import React, { createContext, useState } from "react";
+
+export const Context = createContext();
+
+const ContextProvider = ({ children }) => {
+	const [opened, setIsOpened] = useState(true);
+	return (
+		<div>
+			<Context.Provider value={[opened, setIsOpened]}>
+				{children}
+			</Context.Provider>
+		</div>
+	);
+};
+
+export default ContextProvider;
